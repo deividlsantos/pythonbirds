5a1b8c81e4c0f522992453eac97bcdf7a1ebd755
deividlsantos <deividluiz.santos@hotmail.com>
2018-07-30 17:01:55 -0300

    created complex children attribute

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,19 @@
 class  Pessoa:
-    def __init__(self, nome=None, idade=24):
+    def __init__(self,*filhos,  nome=None, idade=24):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Tatiane')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Deivid'
-    print(p.nome)
-    print(p.idade)
+    deivid = Pessoa(nome='Deivid')
+    tatiane = Pessoa(deivid, nome='Tatiane')
+    print(Pessoa.cumprimentar(tatiane))
+    print(id(tatiane))
+    print(tatiane.cumprimentar())
+    print(tatiane.nome)
+    print(tatiane.idade)
+    for filho in tatiane.filhos:
+        print(filho.nome)
