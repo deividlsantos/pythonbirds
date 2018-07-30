2f2a37f029770a08df9c786c5130fc3c9930535e
deividlsantos <deividluiz.santos@hotmail.com>
2018-07-30 17:32:22 -0300

    created eye class attribute

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class  Pessoa:
+    olhos = 2
+
     def __init__(self,*filhos,  nome=None, idade=24):
         self.idade = idade
         self.nome = nome
@@ -19,5 +21,12 @@ if __name__ == '__main__':
         print(filho.nome)
     tatiane.sobrenome = 'Santos'
     del tatiane.filhos
+    tatiane.olhos = 1
+    del tatiane.olhos
     print(tatiane.__dict__)
     print(deivid.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(tatiane.olhos)
+    print(deivid.olhos)
+    print(id(Pessoa.olhos), id(tatiane.olhos), id(deivid.olhos))
